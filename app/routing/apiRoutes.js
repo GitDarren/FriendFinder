e836a7d2d9c26b5e08e5d6043348b37e45a2cd54
GitDarren <darren321@gmail.com>
2018-02-11 12:30:28 -0600

    Updating ApiRoutes to incorporate logic for buddy finder

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,52 @@
 var path = require('path');
 
+
+var movieBuddy = require('../data/buddy.js');
+
+
 module.exports = function (app) {
-    app.post('/save', function(req, res) {
-        console.log("this is working", req.body);
-        res.send("Hello");
+    //read data from buddy.js
+    app.get('/api/buddy', function(req, res) {
+        res.json(buddyArray);
     });
 
+
+    app.post('/api/buddy', function(req, res)   {
+        var diff = [];
+        var total = 0;
+        var lowestArray = [];
+        var best = 0;
+        var userScores = req.body.scores;
+
+        //loops through each entry in the array
+        for(var i = 0; i <=buddyArray.length; i++)  {
+
+            //Create variable to determine 
+            diff = buddyArray[i].scores.map(function(item, index)  {
+                return Math.abs(item - userScores[index]);
+            })
+
+            total = diff.reduce(function(prev, current)  {
+                return prev + current;
+            }, 0);
+
+            lowestArray.push(total);
+        };
+
+        for(var j = 0; j < lowestArray.length; j++) {
+            if(lowestArray[j] <= lowestArray[best]) {
+                best = j;
+            }
+        }
+
+        res.json(buddyArray[best])
+
+
+        buddyArray.push(req.body)
+
+
     
+    });
 
-
+ 
 }
