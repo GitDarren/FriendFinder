3d758782b4db154a1cb983894a4d43f5ad900ac7
GitDarren <darren321@gmail.com>
2018-02-11 12:52:56 -0600

    Updating buddy.js data file with three dummy users

---
+++ app/data/buddy.js
@@ -0,0 +1,56 @@
+
+
+
+var buddyList = [
+    {
+        "name": "Harry Knowles",
+        "photo": "http://variety.com/2017/film/news/harry-knowles-takes-leave-from-aint-it-cool-news-amid-sexual-assault-accusations-1202574130",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+
+    {
+        "name": "Sally Talksalot",
+        "photo": "http://www.vulture.com/2013/06/david-edelstein-on-texting-and-talking-at-movies.html",
+        "scores": [
+            5,
+            4,
+            3,
+            2,
+            1,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+    },
+
+    {
+        "name": "Miranda Sings",
+        "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.thewrap.com%2Fwp-content%2Fuploads%2F2016%2F01%2FMiranda-Sings.jpg&imgrefurl=https%3A%2F%2Fwww.thewrap.com%2Fyoutube-star-miranda-sings-lands-netflix-comedy-haters-back-off%2F&docid=ona4LD22aRRP-M&tbnid=eNIaX0l2sembEM%3A&vet=10ahUKEwivgrCuw57ZAhUMW60KHdzVBGcQMwjeASgHMAc..i&w=618&h=412&safe=off&bih=1050&biw=1680&q=miranda%20sings&ved=0ahUKEwivgrCuw57ZAhUMW60KHdzVBGcQMwjeASgHMAc&iact=mrc&uact=8",
+        "scores": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+
+]
